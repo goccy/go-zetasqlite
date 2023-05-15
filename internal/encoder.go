4c3b3f5df20c768f836c029e4794980ecc20f052
Colin Adams <colin@recidiviz.org>
2023-05-15 10:03:08 -0700

    Update format when casting datetimes to strings

--- internal/encoder.go
+++ internal/encoder.go
@@ -378,7 +378,25 @@ func CastValue(t types.Type, v Value) (Value, error) {
 			return nil, err
 		}
 		return FloatValue(f64), nil
-	case types.STRING, types.ENUM:
+	case types.STRING:
+		switch v.(type) {
+		// If this is coming from a date/time, the format is slightly different
+		// than when just writing the value out as a string.
+		case DateValue:
+			return convertTimeValueToStringWithFormat(v, "2006-01-02")
+		case DatetimeValue:
+			return convertTimeValueToStringWithFormat(v, "2006-01-02 15:04:05.999999")
+		case TimeValue:
+			return convertTimeValueToStringWithFormat(v, "15:04:05.999999")
+		case TimestampValue:
+			return convertTimeValueToStringWithFormat(v, "2006-01-02 15:04:05.999999-07")
+		}
+		s, err := v.ToString()
+		if err != nil {
+			return nil, err
+		}
+		return StringValue(s), nil
+	case types.ENUM:
 		s, err := v.ToString()
 		if err != nil {
 			return nil, err
@@ -505,6 +523,14 @@ func CastValue(t types.Type, v Value) (Value, error) {
 		return v, nil
 	}
 	return nil, fmt.Errorf("unsupported cast %s value", t.Kind())
+}
+
+func convertTimeValueToStringWithFormat(v Value, format string) (Value, error) {
+	valueTime, err := v.ToTime()
+	if err != nil {
+		return nil, err
+	}
+	return StringValue(valueTime.UTC().Format(format)), nil
 }
 
 func ValueFromGoValue(v interface{}) (Value, error) {
