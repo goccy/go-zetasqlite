922359e0ba170c8bcc9db1815fbc1781133fcee2
Dan Hansen <dan@recidiviz.org>
2024-01-25 14:51:11 -0800

    add tests / fix fractional seconds

--- timestamp.go
+++ timestamp.go
@@ -10,14 +10,19 @@ import (
 // TimeFromTimestampValue zetasqlite returns string values ​​by default for timestamp values.
 // This function is a helper function to convert that value to time.Time type.
 func TimeFromTimestampValue(v string) (time.Time, error) {
-	// ParseFloat is too imprecise to use, instead split into seconds / microseconds
+	// ParseFloat is too imprecise to use, instead split into seconds and fractional seconds
 	parts := strings.Split(v, ".")
 	if len(parts) > 2 {
-		return time.Time{}, fmt.Errorf("cannot parse string with multiple delimiters")
+		return time.Time{}, fmt.Errorf("invalid timestamp string (multiple delimiters) %s", v)
 	}
 	seconds, err := strconv.ParseInt(parts[0], 10, 64)
 	micros := int64(0)
 	if len(parts) == 2 {
+		// Pad fractional places to microseconds i.e. (.1 to 100000 micros)
+		microsString := parts[1]
+		for len(microsString) < 6 {
+			microsString += "0"
+		}
 		micros, err = strconv.ParseInt(parts[1], 10, 64)
 	}
 	if err != nil {
