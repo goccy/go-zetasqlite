4c47e641604caba6a7653fa6854cf0e174685610
Dan Hansen <dan@recidiviz.org>
2024-01-25 14:00:24 -0800

    Avoid floating point errors by using strconv.ParseInt in TimeFromTimestampValue

--- timestamp.go
+++ timestamp.go
@@ -1,18 +1,28 @@
 package zetasqlite
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
-
-	"github.com/goccy/go-zetasqlite/internal"
 )
 
 // TimeFromTimestampValue zetasqlite returns string values ​​by default for timestamp values.
 // This function is a helper function to convert that value to time.Time type.
 func TimeFromTimestampValue(v string) (time.Time, error) {
-	f, err := strconv.ParseFloat(v, 64)
+	// ParseFloat is too imprecise to use, instead split into seconds / microseconds
+	parts := strings.Split(v, ".")
+	if len(parts) > 2 {
+		return time.Time{}, fmt.Errorf("cannot parse string with multiple delimiters")
+	}
+	seconds, err := strconv.ParseInt(parts[0], 10, 64)
+	micros := int64(0)
+	if len(parts) == 2 {
+		micros, err = strconv.ParseInt(parts[1], 10, 64)
+	}
 	if err != nil {
 		return time.Time{}, err
 	}
-	return internal.TimestampFromFloatValue(f)
+	nanos := micros * int64(time.Microsecond)
+	return time.Unix(seconds, nanos), err
 }
