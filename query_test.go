b1554a96dca05408e5ad2897ac409d7d59748e2e
Dan Hansen <dan@recidiviz.org>
2024-01-28 16:22:11 -0800

    [Date] Base date used when parsing is start of unix time, not 0001-01-01

--- query_test.go
+++ query_test.go
@@ -3639,6 +3639,12 @@ WITH example AS (
 			},
 		},
 		{
+			name:  "base date is epoch",
+			query: `SELECT PARSE_DATE("%m", "03")`,
+			expectedRows: [][]interface{}{
+				{"1970-03-01"},
+			}},
+		{
 			name: "extract date",
 			query: `
 SELECT date, EXTRACT(ISOYEAR FROM date), EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date),
