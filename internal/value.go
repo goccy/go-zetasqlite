2c2416b15d9b3cf65bad07e6fc37e2d267e61587
Dan Hansen <dan@recidiviz.org>
2024-03-09 07:03:01 -0800

    [String] Cast to INT64 should use base-10 parsing (#159)
    * [String] Cast to INT64 should use base-10 parsing
    
    * refix 0x
    
    * invert

--- internal/value.go
+++ internal/value.go
@@ -244,7 +244,12 @@ func (sv StringValue) ToInt64() (int64, error) {
 	if sv == "" {
 		return 0, nil
 	}
-	return strconv.ParseInt(string(sv), 0, 64)
+	toParse := string(sv)
+	base := 10
+	if strings.Contains(strings.ToLower(toParse), "0x") {
+		base = 0
+	}
+	return strconv.ParseInt(toParse, base, 64)
 }
 
 func (sv StringValue) ToString() (string, error) {
